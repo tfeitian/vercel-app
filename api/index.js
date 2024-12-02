7b89830256257033af074bbf452655c041416523
tangfei <tangfei@outlook.com>
2024-12-02 20:46:18 +0800

    Change api , add parameters.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-module.exports = (req, res) => {
-  res.status(200).json({message: 'Hello from Vercel!'});
+module.exports = (request, response) => {
+  let who = 'anonymous';
+
+  if (request.body && request.body.who) {
+    who = request.body.who;
+  } else if (request.query.who) {
+    who = request.query.who;
+  } else if (request.cookies.who) {
+    who = request.cookies.who;
+  }
+
+  response.status(200).send(`Hello ${who}!`);
 };
