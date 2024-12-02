293f5dcafce48688b81d767df32a584b46ad0964
tangfei <tangfei@outlook.com>
2024-12-02 20:55:24 +0800

    +test cmds.

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 module.exports = (request, response) => {
   let who = 'anonymous';
 
-  if (request.body && request.body.who) {
+  if (request.body && request.body.who) {  // post with who:45gtg
     who = request.body.who;
-  } else if (request.query.who) {
+  } else if (request.query
+                 .who) {  // get with http://localhost:3000/api?who=fsfgs--
     who = request.query.who;
   } else if (request.cookies.who) {
     who = request.cookies.who;
   }
 
   response.status(200).send(`Hello ${who}!`);
+  // response.json(obj);
 };
