73772b4b4fb41491ca76fabdd545b0837daae7a5
tangfei <tangfei@outlook.com>
2024-12-06 19:35:06 +0800

    Change response.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
+// import * as edgedb from 'edgedb';
+
+// const client = edgedb.createClient({
+//   // Note: these options aren't needed for your project deployed on Vercel,
+//   // they will be automatically found from environment variables
+//   instanceName: 'vercel-WdA2gr3TsoJSJ4o7CAQEKPsP/edgedb-user-storage',
+//   secretKey:
+//       'nbwt1_eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlZGIuZC5hbGwiOnRydWUsImVkYi5pIjpbInZlcmNlbC1XZEEyZ3IzVHNvSlNKNG83Q0FRRUtQc1AvZWRnZWRiLXVzZXItc3RvcmFnZSJdLCJlZGIuci5hbGwiOnRydWUsImlhdCI6MTczMzEzNTIyMSwiaXNzIjoiYXdzLmVkZ2VkYi5jbG91ZCIsImp0aSI6Ijk0MnRETENYRWUtcnBYX0t0SDZTemciLCJzdWIiOiI5emZOa3JDWEVlLThaaHZ0Y0hTVjFnIn0.kcbegUciqff6oXr7lWq4jN0DoGITAzFsiwvfRLfZzrkfSZyiNe9vYDF9GwBywH7zIAbESWsmGYZXeoxy1wHlLQ'
+// });
+
+// const result = await client.query('select 1 + 2');
+
 module.exports = (request, response) => {
   let who = 'anonymous';
 
-  if (request.body && request.body.who) {  // post with who:45gtg
-    who = request.body.who;
+  if (request.body && request.body.email) {  // post with who:45gtg
+    who = request.body.email;
   } else if (request.query
                  .who) {  // get with http://localhost:3000/api?who=fsfgs--
     who = request.query.who;
@@ -10,6 +22,7 @@ module.exports = (request, response) => {
     who = request.cookies.who;
   }
 
-  response.status(200).send(`Hello ${who}!`);
+  response.status(200).send(
+      `Hello ${who} at ${request.body.company} call ${request.body.phone}!`);
   // response.json(obj);
 };
